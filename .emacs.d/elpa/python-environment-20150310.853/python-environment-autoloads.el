2bb0373bad500655f0e78ec9355a6cbd18c296b5
Christopher <chrismulvad@gmail.com>
2019-12-11 00:42:54 +0100

    added internal packages

---
+++ .emacs.d/elpa/python-environment-20150310.853/python-environment-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-environment-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
+;;;;;;  "test-python-environment.el") (23897 50444 285607 401000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-environment-autoloads.el ends here
